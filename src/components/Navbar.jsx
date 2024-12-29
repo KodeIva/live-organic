8b28e7e1dec4546233fa23b66fa6c9956f40ab8e
Iva Karadjova <ivakaradjova@yahoo.co.uk>
2024-12-29 07:21:33 +0000

    logo added

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="container">
+        <span>Live</span>
+        <span>Organic</span>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar   
