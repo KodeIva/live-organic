ff5ddc04616739fe10986b34e72e47aabe37b6da
Iva Karadjova <ivakaradjova@yahoo.co.uk>
2024-12-29 07:56:19 +0000

    menu created

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,34 @@
 import React from 'react'
 import { ImLeaf } from "react-icons/im";
 
+const NavbarMenu = [
+    {
+        id: 1,
+        title: 'Home',
+        link: '/'
+    },
+    {
+        id: 2,
+        title: 'Products',
+        link: '#'
+    },
+    {
+        id: 3,
+        title: 'About',
+        link: '#'
+    },
+    {
+        id: 4,
+        title: 'Shop',
+        link: '#'
+    },
+    {
+        id: 5,
+        title: 'Contacts',
+        link: '#'
+    },
+]
+
 const Navbar = () => {
   return (
     <nav>
@@ -8,6 +36,18 @@ const Navbar = () => {
         <span className='text-[#F72C5B]'>Live </span>
         <span className='text-[#88D66C]'><ImLeaf/></span>
         <span className='text-[#FF885B] ml-[-9px]'>Organic </span>
+      </div>
+
+      <div>
+        <ul>
+            {NavbarMenu.map((menu) => {
+                return (
+                    <li key={menu.id} className='text-xl'>
+                       <a href={menu.link}>{menu.title}</a> 
+                    </li>
+                )
+            })}
+        </ul>
       </div>
     </nav>
   )
