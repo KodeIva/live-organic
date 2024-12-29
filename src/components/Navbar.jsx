a000b91f65aa377f43ee3ac74344352ede31a80d
Iva Karadjova <ivakaradjova@yahoo.co.uk>
2024-12-29 07:47:05 +0000

    logo styled

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,10 +4,10 @@ import { ImLeaf } from "react-icons/im";
 const Navbar = () => {
   return (
     <nav>
-      <div className="container">
-        <span>Live</span>
-        <span>Organic</span>
-        <span><ImLeaf/></span>
+      <div className="container flex items-center text-3xl font-semibold ">
+        <span className='text-[#F72C5B]'>Live </span>
+        <span className='text-[#88D66C]'><ImLeaf/></span>
+        <span className='text-[#FF885B] ml-[-9px]'>Organic </span>
       </div>
     </nav>
   )
