a26742b6719eadb2b3e482d23a670998a4adfee4
Iva Karadjova <ivakaradjova@yahoo.co.uk>
2024-12-29 17:03:46 +0000

    framer-motion installed, added to ResponsiveMenu.jsx

---
+++ src/components/Navbar/ResponsiveMenu.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { motion, AnimatePresence } from 'framer-motion'
+
+const ResponsiveMenu = () => {
+  return (
+    <div>ResponsiveMenu</div>
+  )
+}
+
+export default ResponsiveMenu
