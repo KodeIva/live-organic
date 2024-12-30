a74302502c35ba89c32b97f25ea5114d070cb0f2
Iva Karadjova <ivakaradjova@yahoo.co.uk>
2024-12-30 12:35:18 +0000

    mobile menu added  hover effect and width

--- src/components/Navbar/ResponsiveMenu.jsx
+++ src/components/Navbar/ResponsiveMenu.jsx
@@ -3,16 +3,20 @@ import { motion, AnimatePresence } from 'framer-motion'
 
 const ResponsiveMenu = ({open}) => {
   return (
-    <AnimatePresence>       
+    <AnimatePresence mode='wait'>       
     {
       open &&  <motion.div animate={{}}>
-      <div>
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Services</li>
-          <li>Contact</li>
-        </ul>
+      <div className='absolute top-0 left-0 w-[100%] h-screen z-20'>
+        <div className='text-xl font-semibold bg-[#F72C5B] text-white 
+                        py-10'>
+          <ul className='flex w-full flex-col items-center m-2'>
+            <li className='flex mb-2 w-[50%] justify-center hover:text-[#91e374] hover:translate-x-8 duration-300'><a href="">Home</a></li>
+            <li className='flex mb-2 w-[50%] justify-center hover:text-[#91e374] hover:-translate-x-8 duration-300'>About</li>
+            <li className='flex mb-2 w-[50%] justify-center hover:text-[#91e374] hover:translate-x-8 duration-300'>Services</li>
+            <li className='flex mb-2 w-[50%] justify-center hover:text-[#91e374] hover:-translate-x-8 duration-300'>Contact</li>
+          </ul>
+        </div>
+       
       </div>
     </motion.div>              
     }
