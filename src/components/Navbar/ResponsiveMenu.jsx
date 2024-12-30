0684529b8fb7215a9bfcd21429eb4a6e6c50def4
Iva Karadjova <ivakaradjova@yahoo.co.uk>
2024-12-30 13:01:13 +0000

    div deleted, styles &  onClick() passed to <motion.div>

--- src/components/Navbar/ResponsiveMenu.jsx
+++ src/components/Navbar/ResponsiveMenu.jsx
@@ -5,10 +5,14 @@ const ResponsiveMenu = ({open, setOpen}) => {
   return (
     <AnimatePresence mode='wait'>       
     {
-      open &&  <motion.div animate={{}}>
-      <div className='absolute top-0 left-0 w-full h-screen z-20 duration-700' onClick={() => setOpen(false)}>
-        <div className='text-xl font-semibold bg-[#F72C5B] text-white 
-                        py-10'>
+      open &&  <motion.div 
+         initial={{opacity: 0, y: -100}}
+         animate={{opacity: 1, y: 0}}
+         exit={{opacity: 0, y: -100}}
+         transition={{duration: 0.3}}
+         className='absolute top-0 left-0 w-full h-screen z-20' onClick={() => setOpen(false)}
+         >
+        <div className='text-xl font-semibold bg-[#F72C5B] text-white py-10'>
           <ul className='flex  flex-col items-center m-2'>
             <li className='flex mb-2 w-[50%] justify-center hover:text-[#91e374] hover:translate-x-8 duration-300'><a href="">Home</a></li>
             <li className='flex mb-2 w-[50%] justify-center hover:text-[#91e374] hover:-translate-x-8 duration-300'>About</li>
@@ -16,8 +20,6 @@ const ResponsiveMenu = ({open, setOpen}) => {
             <li className='flex mb-2 w-[50%] justify-center hover:text-[#91e374] hover:-translate-x-8 duration-300'>Contact</li>
           </ul>
         </div>
-       
-      </div>
     </motion.div>              
     }
      </AnimatePresence>
