7aec22ab79ff787a0a2d64e67c24ec6866c9ce37
Iva Karadjova <ivakaradjova@yahoo.co.uk>
2025-01-05 15:42:33 +0000

    images imported and added to FruitData

--- src/utility/data.jsx
+++ src/utility/data.jsx
@@ -1,25 +1,43 @@
+import Raspberries from '../assets/raspberries.png'
+import Oranges from '../assets/orange.png'
+import Lemon from '../assets/lemons.png'
+import Apple from '../assets/apples.png'
+import Kiwi from '../assets/kiwi.png'
+
+
 export const FruitsData = [
     {
         id: 1,
         title: 'Apple',
+        image: Apple,
         link:'/',
         price: '£1.59'
     },
     {
         id: 1,
-        title: 'Kiw',
+        title: 'Kiwi',
+        image: Kiwi,
         link:'/',
         price: '£2.59'
     },
     {
         id: 1,
+        title: 'Lemon',
+        image: Lemon,
+        link:'/',
+        price: '£2.79'
+    },
+    {
+        id: 1,
         title: 'Raspberries',
+        image: Raspberries,
         link:'/',
         price: '£4.00'
     },
     {
         id: 1,
         title: 'Oranges',
+        image: Orange,
         link:'/',
         price: '£2.99'
     }
