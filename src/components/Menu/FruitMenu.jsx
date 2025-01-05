268e55f56483473c66735a00b8751eb56bee3cd5
Iva Karadjova <ivakaradjova@yahoo.co.uk>
2025-01-05 16:45:29 +0000

    responsiveness added

--- src/components/Menu/FruitMenu.jsx
+++ src/components/Menu/FruitMenu.jsx
@@ -4,16 +4,19 @@ import { FruitsData } from '../../utility/data'
 
 const FruitMenu = () => {
   return (
-    <div className='h-auto w-full text-blue-800'>
-         <ul>
+    <div className='bg-slate-400 flex  justify-center items-center h-auto w-full text-blue-800'>
+         <ul className='flex flex-wrap bg-green-300 justify-center items-center w-full'>
         {FruitsData.map((fruit) => {
             return (
-                 
-                <li key={fruit.id}>
-                 <img src={fruit.image} alt={fruit.title} />   
-                 <a href="/">{fruit.title}</a>  
-                 <p>{fruit.price}</p>  
+                
+                <li className='bg-orange-500 h-[200px] flex flex-col justify-center items-center w-[35%] md:w-[30%] lg:w-[20%] m-5
+                 relative' key={fruit.id}>
+                 <img className='w-[120px] pb-6 md:w-[150px] lg:w-[200px]' src={fruit.image} alt={fruit.title} />   
+                 <a className='absolute bottom-8 ' href="/">{fruit.title}</a>  
+                 <p className='absolute bottom-1'>{fruit.price}</p>  
                 </li>
+                
+             
                       
             )
          
