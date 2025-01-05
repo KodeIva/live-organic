4c60417086418b9bbf30ff25e6e3f79d19983c88
Iva Karadjova <ivakaradjova@yahoo.co.uk>
2025-01-05 21:59:15 +0000

    motion, FadeLeft, FadeRight added

--- src/components/Menu/FruitMenu.jsx
+++ src/components/Menu/FruitMenu.jsx
@@ -1,16 +1,27 @@
 import React from 'react'
 import { FruitsData } from '../../utility/data'
-
+import {motion} from 'framer-motion'
+import { FadeRight , FadeLeft} from '../../utility/animation'
 
 const FruitMenu = () => {
   return (
     <div className='bg-slate-50 flex flex-col  justify-center items-center h-auto w-full text-blue-800'>
-        <h1 className='text-3xl text-[#FF885B] my-9 font-semibold'>Organics in our store</h1>
-         <ul className='flex flex-wrap justify-center items-center w-full'>
+        <motion.h1 
+           variants={FadeRight(0.6)}
+           initial='hidden'
+           animate='vissible'
+         className='text-3xl text-[#FF885B] my-9 font-semibold'>Organics in our store</motion.h1>
+         <motion.ul
+            variants={FadeLeft(0.6)}
+            initial='hidden'
+            animate='vissible'
+         className='flex flex-wrap justify-center items-center w-full'>
         {FruitsData.map((fruit) => {
             return (
-                
-                <li 
+                <motion.li 
+                variants={FadeLeft(0.6)}
+                initial='hidden'
+                animate='vissible'
                   className=' h-[200px] flex flex-col justify-center items-center w-[35%] md:w-[30%] lg:w-[20%] m-5
                  relative rounded-3xl shadow-[0_0_22px_0_rgba(0,0,0,0.15)]
                  hover:shadow-[0_0_22px_0_rgba(0,0,0,0.35)]' 
@@ -19,10 +30,10 @@ const FruitMenu = () => {
                   transform-translate-y-6' src={fruit.image} alt={fruit.title} />   
                  <a className='absolute bottom-8 text-xl text-[#FF885B] font-semibold' href="/">{fruit.title}</a>  
                  <p className='absolute bottom-1 text-xl text-[#619a4c] font-semibold'>{fruit.price}</p>  
-                </li> 
+                </motion.li> 
             ) 
         })}
-         </ul> 
+         </motion.ul> 
     </div>
   )
 }
