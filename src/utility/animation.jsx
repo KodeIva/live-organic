b2e3c95a79907028b6afbbe8d2ce682f4293a4ea
Iva Karadjova <ivakaradjova@yahoo.co.uk>
2025-01-05 10:06:44 +0000

    FadeLeft added

--- src/utility/animation.jsx
+++ src/utility/animation.jsx
@@ -15,6 +15,23 @@ export const FadeUp = (delay) => {
     }
 }
 
+export const FadeLeft = (delay) => {
+    return {
+        hidden: {
+            opacity: 0,
+            x: 100
+        },
+        vissible: {
+            opacity: 1,
+            x: 0,
+            transition: {
+                duration: 1,
+                delay: delay
+            }
+        }
+    }
+}
+
 
 export const FadeRight = (delay) => {
     return {
