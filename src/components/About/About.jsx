7795aeb749d25c08b1c6c8f7688f89aa534dd6f6
Iva Karadjova <ivakaradjova@yahoo.co.uk>
2025-01-08 22:38:01 +0000

    button div , about us div styled

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -7,13 +7,13 @@ const About = () => {
   return (
     <section>
       <div className='container grid grid-cols-1 md:grid-cols-2 mt-6 py-14'>
-        <div>
+        <div className='flex justify-center items-center'>
             <img src={OrangeFresh} 
                  alt="orange" 
                  className='w-[300px] md:max-w-[400px] h-full object-cover'
             />
         </div>
-        <div>
+        <div className='flex flex-col  justify-center'>
             <h1>Who We Are</h1>
             <p>
                We are more than just a store, we are committed to promote health,
