6f2a3be0b42176cf8fb078e2d7aa69986f946109
Iva Karadjova <ivakaradjova@yahoo.co.uk>
2025-01-08 22:25:16 +0000

    created <div> with title and about us text

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -13,6 +13,13 @@ const About = () => {
         </div>
         <div>
             <h1>Who We Are</h1>
+            <p>
+               We are more than just a store, we are committed to promote health,
+               sustainability, and ethical choises. We bring organic foods directly 
+               from the earth to your home. Our mission is to make organic living 
+               accessible to everyone, protect the planet and support ethical
+               farming practises
+            </p>
         </div>
       </div>
     </section>
