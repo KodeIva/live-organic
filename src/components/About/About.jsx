68bdfdef15b5e670681bd98c5803ea2c7102d6cc
Iva Karadjova <ivakaradjova@yahoo.co.uk>
2025-01-09 11:38:22 +0000

    motion added to elements

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -8,30 +8,53 @@ const About = () => {
     <section>
       <div className='container grid grid-cols-1 md:grid-cols-2 mt-6 py-14'>
         <div className='flex justify-center items-center'>
-            <img src={OrangeFresh} 
-                 alt="orange" 
-                 className='w-[300px] md:max-w-[400px] h-full object-cover'
+            <motion.img
+              initial={{opacity: 0, scale: 0.5}}
+              whileInView={{opacity: 1, scale: 1}}
+              transition={{type: 'spring', stiffness: 100, delay: 0.2}}
+              viewport={{once: true}}
+              src={OrangeFresh} 
+              alt="orange" 
+              className='w-[300px] md:max-w-[400px] h-full object-cover'
             />
         </div>
-        <div className='flex flex-col  justify-center'>
-            <h1>Who We Are</h1>
-            <p>
+        <div className='flex flex-col  justify-center '>
+          <div className='text-center md:text-left mt-4 lg:max-w-[400px]'>
+            <motion.h1 
+              variants={FadeLeft(0.5)}
+              initial='hidden'
+              whileInView='vissible'
+              viewport={{once: true}}
+              className='text-2xl md:text-4xl font-bold text-slate-500'>Who We Are</motion.h1>
+            <motion.p 
+               variants={FadeLeft(0.6)}
+               initial='hidden'
+               whileInView='vissible'
+               viewport={{once: true}}
+               >
                We are more than just a store, we are committed to promote health,
                sustainability, and ethical choises. We bring organic foods directly 
                from the earth to your home. Our mission is to make organic living 
                accessible to everyone, protect the planet and support ethical
                farming practises
-            </p>
+            </motion.p>
             <motion.div 
                 variants={FadeLeft(0.9)}
                 initial='hidden'
                 animate='vissible'
+                viewport={{once: true}}
                 className='flex justify-center md:justify-center'>
                 
-                <button className='primary-btn flex justify-center align-center gap-2'>
+                <motion.button 
+                   variants={FadeLeft(0.9)}
+                   initial='hidden'
+                   animate='vissible'
+                   viewport={{once: true}}
+                  className='primary-btn flex justify-center align-center gap-2'>
                   Learn More
-                </button>
+                </motion.button>
               </motion.div>
+           </div>
         </div>
       </div>
     </section>
--- src/utility/animation.jsx
+++ src/utility/animation.jsx
@@ -15,6 +15,23 @@ export const FadeDown = (delay) => {
     }
 }
 
+export const FadeUp = (delay) => {
+    return {
+        hidden: {
+            opacity: 0,
+            y: 100
+        },
+        vissible: {
+            opacity: 1,
+            y: 0,
+            transition: {
+                duration: 1,
+                delay: delay
+            }
+        }
+    }
+}
+
 export const FadeLeft = (delay) => {
     return {
         hidden: {
