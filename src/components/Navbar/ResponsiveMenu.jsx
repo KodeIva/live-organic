48366ec7f26397738503f3d34e8fd7878ecf9575
Iva Karadjova <ivakaradjova@yahoo.co.uk>
2025-01-15 21:37:07 +0000

    NavbarMenu imported from data.jsx,Responsive menu rendered with map()

--- src/components/Navbar/ResponsiveMenu.jsx
+++ src/components/Navbar/ResponsiveMenu.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { motion, AnimatePresence } from 'framer-motion'
+import { NavbarMenu } from '../../utility/data'
 
 const ResponsiveMenu = ({open, setOpen}) => {
   return (
@@ -14,10 +15,15 @@ const ResponsiveMenu = ({open, setOpen}) => {
          >
         <div className='text-xl font-semibold bg-[#F72C5B] text-white py-10 cursor-pointer'>
           <ul className='flex  flex-col items-center m-2'>
-            <li className='flex mb-2 w-[50%] justify-center hover:text-[#91e374] hover:translate-x-8 duration-300'><a href="">Home</a></li>
-            <li className='flex mb-2 w-[50%] justify-center hover:text-[#91e374] hover:-translate-x-8 duration-300'>About</li>
-            <li className='flex mb-2 w-[50%] justify-center hover:text-[#91e374] hover:translate-x-8 duration-300'>Services</li>
-            <li className='flex mb-2 w-[50%] justify-center hover:text-[#91e374] hover:-translate-x-8 duration-300'>Contact</li>
+          {NavbarMenu.map((menu) => {
+                return (
+                    <li key={menu.id}  className='flex mb-2 w-[50%] justify-center hover:text-[#91e374] hover:translate-x-8 duration-300'>
+                       <a href={menu.link}>
+                         {menu.title}
+                       </a> 
+                    </li>
+                )
+            })}
           </ul>
         </div>
     </motion.div>              
