dccf80bc48c49d09124f1fd708b69eb118506c00
Iva Karadjova <ivakaradjova@yahoo.co.uk>
2025-01-19 21:59:01 +0000

    Added useEffect and function 'randomRecipe' ,fetch(url) working - returns random recipe

--- src/components/Recipes/RandomRecipe.jsx
+++ src/components/Recipes/RandomRecipe.jsx
@@ -5,6 +5,16 @@ const url = 'https://www.themealdb.com/api/json/v1/1/random.php'
 const RandomRecipe = () => {
   const [randomMeal, setRandomMeal] = useState([])
 
+  useEffect(() => {
+    const randomRecipe = async () => {
+        const response = await fetch(url)
+        const randomMeal = await response.json()
+        setRandomMeal(randomMeal)
+        console.log(randomMeal);
+    } 
+    randomRecipe()
+  },[])
+
 
   return (
     <div id='recipe'>RandomRecipe</div>
