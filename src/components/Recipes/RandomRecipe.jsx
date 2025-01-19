c20423d07e64e33935181214ef42c91555b3d783
Iva Karadjova <ivakaradjova@yahoo.co.uk>
2025-01-19 22:02:51 +0000

    Added try/catch block inside function

--- src/components/Recipes/RandomRecipe.jsx
+++ src/components/Recipes/RandomRecipe.jsx
@@ -3,14 +3,19 @@ import { useState, useEffect} from 'react'
 const url = 'https://www.themealdb.com/api/json/v1/1/random.php'
 
 const RandomRecipe = () => {
-  const [randomMeal, setRandomMeal] = useState([])
+  const [meal, setRandomMeal] = useState([])
 
   useEffect(() => {
     const randomRecipe = async () => {
+     try{  
         const response = await fetch(url)
         const randomMeal = await response.json()
         setRandomMeal(randomMeal)
         console.log(randomMeal);
+
+     }catch(error) {
+       console.log(error);
+     }
     } 
     randomRecipe()
   },[])
