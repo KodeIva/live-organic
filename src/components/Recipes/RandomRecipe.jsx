e7223f910dd8701e1979ed93848e422d9a7ddb37
Iva Karadjova <ivakaradjova@yahoo.co.uk>
2025-01-31 16:32:02 +0000

    Working data back from fetch

--- src/components/Recipes/RandomRecipe.jsx
+++ src/components/Recipes/RandomRecipe.jsx
@@ -3,15 +3,16 @@ import { useState, useEffect} from 'react'
 const url = 'https://www.themealdb.com/api/json/v1/1/random.php'
 
 const RandomRecipe = () => {
-  const [meal, setRandomMeal] = useState([])
+  const [meal, setRandomMeal] = useState({})
 
   useEffect(() => {
     const randomRecipe = async () => {
      try{  
         const response = await fetch(url)
         const randomMeal = await response.json()
-        setRandomMeal(randomMeal)
+        setRandomMeal(randomMeal.meals[0])
         console.log(randomMeal);
+        console.log(randomMeal.meals[0].strMeal);
 
      }catch(error) {
        console.log(error);
@@ -22,7 +23,16 @@ const RandomRecipe = () => {
 
 
   return (
-    <div id='recipe'>RandomRecipe</div>
+    <div id='recipe' className='bg-blue-50'>
+        <h1>{meal.strMeal}</h1>
+        <img src={meal.strMealThumb} alt="" />
+        <p>
+            <a href={meal.strSource}>Recipe and instructions</a>
+        </p>
+        <p><span>{meal.strMeasure1}</span><span>{meal.strIngredient1}</span></p>
+
+        
+    </div>
   )
 }
 
