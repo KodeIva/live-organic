ee5785b8f02603554cfa9fa5e78d09c9c4817e86
Iva Karadjova <ivakaradjova@yahoo.co.uk>
2025-03-06 14:04:15 +0000

    Logo, currentYear and link added

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { ImLeaf } from "react-icons/im";
+
+let currentYear = new Date().getFullYear()
 
 const Footer = () => {
   return (
-    <div>Hi I'm the footer</div>
+    <div className='m-10'>
+      <div className="container flex items-center text-xl font-semibold ">
+        <span className='text-[#F72C5B]'>Live </span>
+        <span className='text-[#619a4c]'><ImLeaf/></span>
+        <span className='text-[#FF885B] ml-[-9px]'>Organic </span>
+      </div>
+      <div>
+      <span> &#169;{currentYear}</span>
+      <span> Created by 
+        <a href="https://kodeiva.netlify.app"> KodeIva</a> 
+      </span>
+      </div>
+    </div>
   )
 }
 
