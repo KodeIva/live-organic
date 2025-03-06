7e451dc2f4f1afb23d6ca25d0ec42a037b61d129
Iva Karadjova <ivakaradjova@yahoo.co.uk>
2025-03-06 15:02:44 +0000

    <ul> with menu links added to footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,12 +2,27 @@ import React from 'react'
 import { ImLeaf } from "react-icons/im";
 import { FaFacebookSquare, FaYoutube,FaPinterestSquare, FaInstagramSquare} from "react-icons/fa";
 import { FaSquareXTwitter,  } from "react-icons/fa6";
+import { NavbarMenu } from '../../utility/data';
 
 let currentYear = new Date().getFullYear()
 
 const Footer = () => {
   return (
     <div className='m-10'>
+      <div>
+      <ul className='flex items-center gap-6 text-gray-600'>
+            {NavbarMenu.map((menu) => {
+                return (
+                    <li key={menu.id} className='text-xl'>
+                       <a href={menu.link} className='inline-block py-1 px-3 hover:text-[#619a4c]
+                           hover:shadow-[0_3px_0_-1px_#619a4c] font-semibold'>
+                         {menu.title}
+                       </a> 
+                    </li>
+                )
+            })}
+        </ul>
+      </div>
       <div>
          <div className="container flex items-center text-xl font-semibold ">
           <span className='text-[#F72C5B]'>Live </span>
@@ -17,8 +32,9 @@ const Footer = () => {
          <div>
            <span> &#169;{currentYear}</span>
            <span> Created by 
-             <a href="https://kodeiva.netlify.app"> KodeIva</a> 
+             <a href="https://kodeiva.netlify.app"> KodeIva. </a> 
            </span>
+           <span> All rights reserved.</span>
          </div>
       </div>
 
