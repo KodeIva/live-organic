25e8a4ec4749b12e9c5a609237840bb4071299b6
Iva Karadjova <ivakaradjova@yahoo.co.uk>
2025-03-13 16:32:17 +0000

    Contact.jsx imported and added to App.js

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import FruitMenu from './components/Menu/FruitMenu'
 import  About  from './components/About/About'
 import RandomRecipe from './components/Recipes/RandomRecipe'
 import Footer from './components/Footer/Footer'
+import Contact from './components/Contact/Contact'
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
       <FruitMenu/>
       <About/>
       <RandomRecipe/>
+      <Contact/>
       <Footer/>
     </main>
   )
