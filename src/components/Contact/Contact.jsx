980712ecf9832344423b4e83da366eb0c3fbb7b7
Iva Karadjova <ivakaradjova@yahoo.co.uk>
2025-03-13 18:44:09 +0000

    label, input and textarea added in a <form>

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -4,6 +4,13 @@ const Contact = () => {
   return (
     <section id="contacts">
         <h2>Send an email</h2>
+        <form action="">
+            <label htmlFor="email"></label>
+            <input type="email" placeholder="Email" />
+            <label htmlFor="name"></label>
+            <input type="text" />
+            <textarea name="message" id="message" cols="30" rows="10" placeholder="Your message..."></textarea>
+        </form>
     </section>
   )
 }
