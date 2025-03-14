36fea4f0c5bd3af3ebc08e0803212d577e85eeb2
Iva Karadjova <ivakaradjova@yahoo.co.uk>
2025-03-14 18:19:48 +0000

    Formspree endpoint and method  added to form,button to submit form created

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -4,12 +4,18 @@ const Contact = () => {
   return (
     <section id="contacts">
         <h2>Send an email</h2>
-        <form action="">
+        <form action="https://formspree.io/f/manenyqa" method="POST">
             <label htmlFor="email"></label>
-            <input type="email" placeholder="Email" />
+            <input type="email" placeholder="Your Email" />
             <label htmlFor="name"></label>
             <input type="text" />
-            <textarea name="message" id="message" cols="30" rows="10" placeholder="Your message..."></textarea>
+            <textarea 
+           
+              name="message" 
+              id="message" cols="30" rows="10" 
+              placeholder="Your message...">
+            </textarea>
+            <button type="submit">Send</button>
         </form>
     </section>
   )
