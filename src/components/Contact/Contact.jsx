506a83acc6031d54f16616ce2ee39b13b1a87fbd
Iva Karadjova <ivakaradjova@yahoo.co.uk>
2025-03-17 18:38:45 +0000

    <label> and <input> for Name created

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -5,8 +5,10 @@ const Contact = () => {
     <section className="bg-white flex flex-col m-2 p-4 justify-center items-center"  id="contacts">
         <h2>Get in Touch</h2>
         <form className="bg-blue-50 flex flex-col w-[90%] md:w-[60%] lg:w-[40%] h-auto " action="https://formspree.io/f/manenyqa" method="POST">
+        <label htmlFor="email"></label>
+            <input className="bg-slate-400 text-white placeholder-white" type="email" placeholder="Name" />
             <label htmlFor="email"></label>
-            <input className="bg-slate-400 text-white placeholder-white" type="email" placeholder="Your email" />
+            <input className="bg-slate-400 text-white placeholder-white" type="email" placeholder="Email" />
             <label htmlFor="name"></label>
             <input type="text" />
             <textarea 
