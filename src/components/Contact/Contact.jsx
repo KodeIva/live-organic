517c95ebe19abea3d9f0b278e3afbdfdce427129
Iva Karadjova <ivakaradjova@yahoo.co.uk>
2025-03-17 18:36:00 +0000

    display,background, placeholder color

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -2,15 +2,15 @@
 
 const Contact = () => {
   return (
-    <section id="contacts">
-        <h2>Send an email</h2>
-        <form action="https://formspree.io/f/manenyqa" method="POST">
+    <section className="bg-white flex flex-col m-2 p-4 justify-center items-center"  id="contacts">
+        <h2>Get in Touch</h2>
+        <form className="bg-blue-50 flex flex-col w-[90%] md:w-[60%] lg:w-[40%] h-auto " action="https://formspree.io/f/manenyqa" method="POST">
             <label htmlFor="email"></label>
-            <input type="email" placeholder="Your Email" />
+            <input className="bg-slate-400 text-white placeholder-white" type="email" placeholder="Your email" />
             <label htmlFor="name"></label>
             <input type="text" />
             <textarea 
-           
+              className="bg-slate-400 text-white placeholder-white"
               name="message" 
               id="message" cols="30" rows="10" 
               placeholder="Your message...">
