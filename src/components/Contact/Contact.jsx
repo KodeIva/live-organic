9d548de0af49b7fd53c6f9222e7cabf7cfa030da
Iva Karadjova <ivakaradjova@yahoo.co.uk>
2025-03-19 19:38:45 +0000

    <label> and <input> styled

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -4,17 +4,17 @@ const Contact = () => {
   return (
     <section className="bg-white flex flex-col m-2 p-4 justify-center items-center"  id="contacts">
         <h2>Get in Touch</h2>
-        <form className="bg-blue-50 flex flex-col w-[90%] md:w-[60%] lg:w-[40%] h-auto " action="https://formspree.io/f/manenyqa" method="POST">
+        <form className="flex flex-col w-[90%] md:w-[60%] lg:w-[40%] h-auto " action="https://formspree.io/f/manenyqa" method="POST">
         <label htmlFor="email"></label>
-            <input className="bg-slate-400 text-white placeholder-white" type="email" placeholder="Name" />
+            <input className="bg-slate-400 outline-none h-10 my-2 pl-2 rounded-lg text-white placeholder-white" type="email" placeholder="Name" />
             <label htmlFor="email"></label>
-            <input className="bg-slate-400 text-white placeholder-white" type="email" placeholder="Email" />
+            <input className="bg-slate-400 outline-none h-10 my-2 pl-2 rounded-lg text-white placeholder-white" type="email" placeholder="Email" />
             <label htmlFor="name"></label>
             <input type="text" />
             <textarea 
-              className="bg-slate-400 text-white placeholder-white"
+              className="bg-slate-400 outline-none text-white placeholder-white pl-2 pt-2 rounded-lg"
               name="message" 
-              id="message" cols="30" rows="10" 
+              id="message" cols="10" rows="8" 
               placeholder="Your message...">
             </textarea>
             <button type="submit">Send</button>
